9e7600ca028573f80b4de14450319ed93279faaf
vivek singh <cu.vivek.1557@gmail.com>
2023-08-22 12:41:24 +0530

    access modifiers

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 class Datas<S, N, B> {
-  name: S;
-  age: N;
+  public name: S;
+  protected age: N;
   gender: S;
   adult: B;
+  private salary:N
   constructor(name: S, age: N, gender: S, adult: B) {
     this.name = name;
     this.age = age;
@@ -11,9 +12,10 @@ class Datas<S, N, B> {
   }
 }
 
-class Emp<S, N, B> extends Datas<S, N, B> {
+class Emp<S, N, B> extends Datas<S, N, B> {  //inheritance
   address: S;
   constructor(name: S, age: N, gender: S, adult: B, address: S) {
+    // this.salary; can not access the salary because it is privat 
     super(name, age, gender, adult);
     this.address = address;
   }
@@ -26,12 +28,13 @@ const person: Datas<string, number, boolean> = new Datas(
   true
 );
 const Empe: Emp<string, number, boolean> = new Emp(
-  "Vivek",
-  24,
+  "Rajat",
+  26,
   "male",
   true,
   "Bihar"
 );
 
-console.log(person);
+console.log(person.name);
+//console.log(person.age);  //can not access protected
 console.log(Empe);
